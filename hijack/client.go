1a5b8c576e4fd915e94feff5addcde8a15127fd3
navigaid <navigaid@gmail.com>
2019-12-30 03:10:28 +0800

    add hijack example

---
+++ hijack/client.go
@@ -0,0 +1,67 @@
+// +build client
+
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+
+	"github.com/yrpc/yrpc"
+)
+
+const (
+	Ping yrpc.Cmd = iota
+	Pong
+)
+
+func main() {
+	conn, err := net.Dial("tcp", ":8000")
+	if err != nil {
+		panic(err)
+	}
+	conn.Write([]byte("GET /hijack HTTP/1.1\r\nHost: localhost:8000\r\n\r\n"))
+
+	ioutil.ReadAll(io.LimitReader(conn, 1))
+
+	/*
+	handler := yrpc.HandlerFunc(func(w yrpc.FrameWriter, r *yrpc.RequestFrame) {
+		log.Println("Handler invoked")
+		for {
+			f := <-r.FrameCh()
+		}
+	})
+	*/
+
+	conf := yrpc.ClientConfig{
+		OverlayNetwork: func(addr string, dc yrpc.DialConfig) (net.Conn, error) { return conn, nil },
+		// Handler:        handler,
+	}
+
+	yconn, _ := yrpc.NewConnection("-", conf, nil)
+	log.Println("sending Ping")
+
+	// _, resp, _ := yconn.Request(Ping, 0, nil)
+	w, resp, _ := yconn.StreamRequest(Ping, 0, nil)
+	w.StartWrite(Ping)
+	w.EndWrite(false)
+	frame, _ := resp.GetFrame()
+	log.Println("Response received", string(frame.Payload))
+	for {
+		f := <-frame.FrameCh()
+		payload := string(f.Payload)
+		cmd := ""
+		switch f.Cmd {
+		case Ping:
+			cmd = "ping"
+		default:
+			cmd = "unknown cmd"
+		}
+		log.Println(cmd, payload)
+		w.StartWrite(Pong)
+		w.WriteBytes([]byte(payload))
+		w.EndWrite(false)
+	}
+	select {}
+}
---
+++ hijack/server.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/btwiuse/conntroll/pkg/wrap"
+	"github.com/yrpc/yrpc"
+)
+
+// lys implements net.Listener
+type lys struct {
+	conns chan net.Conn
+}
+
+func (l *lys) Handler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("hijacking /hijack")
+		conn, _ := wrap.Hijack(w)
+		log.Println("hijacked /hijack, sending conn to l.conns")
+		l.conns <- conn
+		conn.Write([]byte(" "))
+		log.Println("sent")
+	})
+}
+
+func (l *lys) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func (l *lys) Accept() (net.Conn, error) {
+	return (<-l.conns).(*wrap.Conn).TCPConn, nil
+}
+
+func (l *lys) Close() error {
+	return nil
+}
+
+func (l *lys) Addr() net.Addr {
+	return l
+}
+
+func (l *lys) Network() string {
+	return "hijack"
+}
+
+func (l *lys) String() string {
+	return l.Network()
+}
+
+func NewLys() *lys {
+	return &lys{
+		conns: make(chan net.Conn, 100),
+	}
+}
+
+const (
+	Ping yrpc.Cmd = iota
+	Pong
+)
+
+func main() {
+	ly := NewLys()
+	mux := http.NewServeMux()
+	mux.Handle("/hijack", ly.Handler())
+
+	ymux := yrpc.NewServeMux()
+	ymux.Handle(Ping, yrpc.HandlerFunc(func(w yrpc.FrameWriter, r *yrpc.RequestFrame) {
+		w.StartWrite(r.RequestID, Pong, yrpc.StreamFlag)
+		w.WriteBytes([]byte("stream begins"))
+		w.EndWrite()
+
+		// consule the first resp
+		<-r.FrameCh()
+
+		quit := make(chan struct{})
+		go func (){
+			for {
+				f := <-r.FrameCh()
+				if f == nil {
+					log.Println("nil chan")
+					close(quit)
+					return
+				}
+				log.Println("client response:", string(f.Payload))
+			}
+		}()
+
+		for i := 0; ; i++ {
+			select {
+			case <-quit:
+				break
+			default:
+				payload := fmt.Sprintf("server request %d", i)
+				w.StartWrite(r.RequestID, Ping, yrpc.StreamFlag)
+				w.WriteBytes([]byte(payload))
+				w.EndWrite() // send frame
+				log.Println(payload)
+			}
+			time.Sleep(time.Second)
+		}
+	}))
+
+	ys := yrpc.NewServer(yrpc.ServerConfig{
+		ListenFunc: func(network, addr string) (net.Listener, error) { return ly, nil },
+		Handler:    ymux,
+	})
+	go func() {
+		ys.ListenAndServe()
+	}()
+
+	http.ListenAndServe(":8000", mux)
+}
